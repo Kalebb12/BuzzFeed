de13764bde9199650e4fe7b5c19e2f9e0bfed4f1
Caleb <2021.cee.jay@gmail.com>
2024-06-23 08:22:47 +0100

    fixed fetch request

--- src/components/newsBoard/newsBoard.jsx
+++ src/components/newsBoard/newsBoard.jsx
@@ -4,7 +4,7 @@ import { NewsItem } from "../newsItem/newsItem";
 export const NewsBoard = ({category}) => {
     const [article,setArticle]=useState([])
     useEffect(()=>{
-        let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
        fetch(url)
        .then(res=>{return res.json()})
        .then(data =>setArticle(data.articles))
@@ -14,7 +14,7 @@ export const NewsBoard = ({category}) => {
             <h2 className="text-center">latest <span className="badge bg-danger fs-4">NEWS</span></h2>
             <div className="container-xl">
                 {
-                    article.map((NEWS,index)=>{
+                   article && article.map((NEWS,index)=>{
                         return(
                             <NewsItem key={index} news={NEWS}/>
                         )
